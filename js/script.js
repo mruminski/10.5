d3de9738ee762f4ca7cebf3e6e9008ab34fc362e
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-25 23:05:15 +0200

    Clean js file

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
-'use strict'; 
-(function(){ 
-		var accordionVanillaJS = function(){
-		
-		var headers = document.querySelectorAll('#accordion-vanillajs h3');
-		
-		var headerClickCallback = function(){
-			this.classList.add('active');
-			
-			for(var i = 0; i < headers.length; i++){
-				if(headers[i].classList.contains('active') && headers[i] != this){
-					headers[i].classList.remove('active');
-				}
-			}
-		};
-	
-		for(var i = 0; i < headers.length; i++){
-			headers[i].addEventListener('click', headerClickCallback);
-		}
-	};
-	
-	accordionVanillaJS();
+"use strict";
+(function() {
+  var accordionVanillaJS = function() {
+    var headers = document.querySelectorAll("#accordion-vanillajs h3");
 
-	var accordionJQuery = function(){
-		
-	};
-	
-	accordionJQuery();
-	
-	
-})(); 
+    var headerClickCallback = function() {
+      this.classList.add("active");
+
+      for (var i = 0; i < headers.length; i++) {
+        if (headers[i].classList.contains("active") && headers[i] != this) {
+          headers[i].classList.remove("active");
+        }
+      }
+    };
+
+    for (var i = 0; i < headers.length; i++) {
+      headers[i].addEventListener("click", headerClickCallback);
+    }
+  };
+
+  accordionVanillaJS();
+
+  var accordionJQuery = function() {
+    
+  };
+
+  accordionJQuery();
+})();
